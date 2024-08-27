9d1563ca1bad785fbc7725d91e16a29001131453
llturtle22ll <angelopazin@gmail.com>
2024-08-27 16:35:19 -0300

    deleted junk

--- src/include/reader.c
+++ src/include/reader.c
@@ -49,19 +49,13 @@ void read_elf(char* elf_file) {
         fprintf(stderr, "elf_getshdrstrndx failed: %s\n", elf_errmsg(-1));
     }
 
-
-
     if ((i = gelf_getclass(e)) == ELFCLASSNONE) {
         errx(EX_SOFTWARE, "getclass() failed: %s.", elf_errmsg (-1));
     }
 
-    // printf("%s: %d - bit ELF object\n", elf_file, i == ELFCLASS32 ? 32 : 64);
-
     if ((id = elf_getident(e, NULL)) == NULL) {
         errx(EX_SOFTWARE , "getident() failed: %s.", elf_errmsg ( -1));
     }
-
-    // printf("%3s e_ident [0..%1d] %7s", " ", EI_ABIVERSION, "\n");
 
     while ((scn = elf_nextscn(e, scn)) != NULL) {
         if(!gelf_getshdr(scn, &shdr)) {
@@ -93,27 +87,6 @@ void read_elf(char* elf_file) {
             break;
         }
     }
-
-    // while ((scn = elf_nextscn(e, scn)) != NULL) {
-    //     gelf_getshdr(scn, &shdr);
-    //     char *section_name = elf_strptr(e, elf_ndxscn(scn), shdr.sh_name);
-
-    //     // Look for the .text section where instructions are usually stored
-    //     if (strcmp(section_name, ".text") == 0) {
-    //         data = elf_getdata(scn, data);
-    //         if (data == NULL || data->d_buf == NULL) {
-    //             fprintf(stderr, "Failed to read .text section\n");
-    //             exit(EXIT_FAILURE);
-    //         }
-
-    //         // Print the instructions in hex
-    //         for (size_t i = 0; i < data->d_size; i += 4) {
-    //             uint32_t instruction = *(uint32_t *)((uint8_t *)data->d_buf + i);
-    //             printf("%08x\n", instruction);
-    //         }
-    //     }
-    // }
-
     elf_end(e);
     close(fd);
 }
